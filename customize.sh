05a0b08a89a105efdb82fdd59e28ace46c20da0d
tomppi <60854909+tomppi@users.noreply.github.com>
2026-07-14 14:03:21 +0300

    Fix installer customize script for v7.1

--- customize.sh
+++ customize.sh
@@ -1,4 +1,9 @@
 #!/system/bin/sh
-ui_print "Installing Q5Q NFC SN220 vendor fix v5 - correct RF conf location"
-ui_print "This version does NOT replace /vendor/etc/libnfc-nxp.conf."
-ui_print "It provides /vendor/libnfc-nxp_RF.conf and /vendor/lib64/libsn220u_fw.so."
+ui_print "Installing Q5Q NFC SN220 vendor RF config fix v7.1"
+ui_print "Keeps the ROM /vendor/etc/libnfc-nxp.conf unchanged."
+ui_print "Provides the q5q SN220 RF file for the HAL path:"
+ui_print "  /system/vendor/libnfc-nxp_RF.conf"
+ui_print "If KernelSU cannot create that vendor-root file, use Mountify/overlayfs with:"
+ui_print "  source: /data/adb/modules/q5q_nfc_sn220_vendor_fix/system"
+ui_print "  target: /system"
+ui_print "Do not install GitHub source zips directly; use the workflow-built module zip."
