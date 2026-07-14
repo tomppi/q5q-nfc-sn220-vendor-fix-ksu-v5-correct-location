13a1b43be43c1a35956d01e463f83e12c096b4de
tomppi <60854909+tomppi@users.noreply.github.com>
2026-07-14 14:03:56 +0300

    Document external overlay cleanup on uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,5 @@
 #!/system/bin/sh
-# No persistent files are written by this module.
+# No persistent vendor/system files are written by this module.
+# Remove any external Mountify/overlayfs rule separately if you created one.
+rm -f /data/adb/modules/q5q_nfc_sn220_vendor_fix/bindmount.log 2>/dev/null
 exit 0
